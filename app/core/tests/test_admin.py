2d74ed3c648debb519770af11cf1b12ee3646bef
MrMobbi <julliatmathias06@gmail.com>
2024-02-01 17:54:52 +0100

    course 57

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -1,5 +1,5 @@
 """
-Test fot the Django admin modification.
+Test for the Django admin modification.
 """
 
 from django.test import TestCase
@@ -32,3 +32,10 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test the edit user page works."""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
