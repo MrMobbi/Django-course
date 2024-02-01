4ff3145f552a10f5a4b2af6ca00b09975ccc8582
MrMobbi <julliatmathias06@gmail.com>
2024-02-01 17:27:04 +0100

    course 56

--- app/core/admin.py
+++ app/core/admin.py
@@ -5,7 +5,7 @@ Django admin customization.
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from core.models import models
+from core import models
 
 
 class UserAdmin(BaseUserAdmin):
