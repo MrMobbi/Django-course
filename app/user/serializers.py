e090dfeb5eeb587611a60a3221d7b36a01af06a5
MrMobbi <julliatmathias06@gmail.com>
2024-02-02 16:27:30 +0100

    course 71

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -26,7 +26,7 @@ class UserSerializer(serializers.ModelSerializer):
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user auth token."""
-    email = serializers.emailField()
+    email = serializers.EmailField()
     password = serializers.CharField(
         style={'input_type': 'password'},
         trim_whitespace=False,
@@ -35,14 +35,14 @@ class AuthTokenSerializer(serializers.Serializer):
         """Validate and authenticate the user."""
         email = attrs.get('email')
         password = attrs.get('password')
-        user = authtenticate(
+        user = authenticate(
             request=self.context.get('request'),
             username=email,
             password=password,
         )
         if not user:
             msg = _('Unable to authenticate with provided credential')
-            raise serializer.ValidationError(msg, code='authorization')
+            raise serializers.ValidationError(msg, code='authorization')
 
         attrs['user'] = user
         return attrs
